ea1f16dba8f480c18b8cf1148d2a303413fcdf64
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2022-11-03 23:29:29 -0500

    add sc

--- resources/[vorp_plugins]/[external]/GoldPaning/server/main.lua
+++ resources/[vorp_plugins]/[external]/GoldPaning/server/main.lua
@@ -15,7 +15,7 @@ AddEventHandler("search", function()
     local item = "goldnugget"
     local r = math.random(1,10)
     local _source = source 
-    if r < 3 then
+    if r < 6 then
         VORP.addItem(_source, item,math.random(1,3))
         TriggerClientEvent("vorp:TipBottom", _source, Config.oro_encontrado, 6000)
     else
