f88d1615e260a1fc7bb8c44740c60483c118ccc5
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2022-11-04 12:53:39 -0500

    add sc

--- resources/[vorp_essentials]/vorp_inventory/sql/weapons.sql
+++ resources/[vorp_essentials]/vorp_inventory/sql/weapons.sql
@@ -54,3 +54,6 @@ INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VA
 INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('ammovolmolotov', 'Volatile Molotov Ammo', 10, 1, 'item_standard', 1);
 
 INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('carrots', 'Zanahoria', 50, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('cigar', 'Tabaco', 50, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('cigarette', 'Cigarro', 50, 1, 'item_standard', 1);
+
