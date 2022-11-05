27bb9322baaf26813b415cdb41c7899d7ca2e6f8
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2022-11-04 21:51:04 -0500

    add sc

---
+++ resources/[gamemodes]/[maps]/EmeraldRanchHouse/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "adamant"
+game "rdr3"
+lua54 'yes'
+
+this_is_a_map "yes"
+rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
+
+files {
+  "stream/House.ymap",
+  "stream/HouseInsides.ymap",
+}
+--Spawn Location - vector4(1459.343, 320.0804, 81.81499, 250.0418)
+
