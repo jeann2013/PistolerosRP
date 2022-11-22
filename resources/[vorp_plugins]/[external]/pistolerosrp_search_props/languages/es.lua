733f2aa2a0d1cccf164bb07c624a667593c50ef1
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2022-11-22 16:55:40 -0500

    add new script

---
+++ resources/[vorp_plugins]/[external]/pistolerosrp_search_props/languages/es.lua
@@ -0,0 +1,12 @@
+--------------------------------------------------------------------------------------
+---------------------------------- ENGLISH -------------------------------------------
+
+Locales["es"] = {
+
+  prompt = "Presione",
+  alreadySearched = "Parece que alguien ya ha buscado aqui",
+  found = "Has encontrado algo y lo has tomado",
+  nothingFound = "No has encontrado nada",
+  searching= "Buscando..."
+ 
+}
