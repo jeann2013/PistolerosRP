b31d50ceafd515dc5f8da444d340641a9fcb8388
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2022-11-22 23:13:20 -0500

    add

--- resources/[vorp_plugins]/vorp_hunting/client/main.lua
+++ resources/[vorp_plugins]/vorp_hunting/client/main.lua
@@ -75,7 +75,7 @@ function awardQuality(quality, entity, horse, cb)
     if not skinFound then
         --TriggerEvent("vorp:TipRight", Config.Language.NotInTheButcher, 4000) -- Notification when the animal isn't being sold in the butcher 
     else
-        TriggerServerEvent("vorp_hunting:giveReward", "pelt", {model=skinFound,quality=quality,entity=entity,horse=horse}, false)
+        --TriggerServerEvent("vorp_hunting:giveReward", "pelt", {model=skinFound,quality=quality,entity=entity,horse=horse}, false)
 		cb()
     end
 end
