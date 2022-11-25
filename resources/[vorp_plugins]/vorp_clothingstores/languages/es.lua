3541c5f98c2113b32b39e80bc95b5c46ede14ca3
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2022-11-24 19:28:55 -0500

    add

---
+++ resources/[vorp_plugins]/vorp_clothingstores/languages/es.lua
@@ -0,0 +1,105 @@
+--------------------------------------------------------------------------------------
+---------------------------------- ENGLISH -------------------------------------------
+--------------------------------------------------------------------------------------
+-------------- DO NOT change any values that are not enclosed in commas --------------
+--------------------------------------------------------------------------------------
+
+Locales["Es"] = {
+    PressToOpen = "Presione [~e~Space~q~] para acceder a la tienda", 	-- Prompt when in range of tailor
+    PressGuide = "Girar <-[A] [D]-> | Cameras [W] o [S]",		-- Prompt inside of clothing store
+	CostOverlay = "Total: $",
+	LoadingOverlay = "Cargando ...",
+    TitleMenuOutfitsUseBtn = "Llevar Ropa",
+    TitleMenuOutfitsDeleteBtn = "Borrar Ropa",
+
+    TitleMenuClothes = "Comprar ropa",
+    SubTitleMenuClothes = "Viste a tu personaje",
+
+    NoExistValue = "No existe", -- Unused
+
+    NoMoney = "No tienes suficiente dinero",
+    SuccessfulBuy = "Te han cobrado",
+
+    TitleMenuOutfits = "Ropas",
+    SubTitleMenuOutfits = "Selecciona entre tus ropas guardadas",
+
+    PlaceHolderNewOutfit = "Ingrese nombre para guardar",
+    ButtonNewOutfit = "Guardar",
+
+    PlaceHolderSpecifyComp = "Ingresa #0-",			-- Disregard, for upcoming update
+    ButtonSpecifyComp = "Ir a",						-- Disregard, for upcoming update
+	WrongInputType = "Solo se permiten números",	-- Disregard, for upcoming update
+	OutOfRange = "El valor está fuera de rango, máx.: ",	-- Disregard, for upcoming update
+	
+    Finish = "Compra completa",
+	SelectOutfit = "Confirmar selección de ropa",
+	FinishDesc = "Asegúrate de haber elegido todo correctamente.",
+
+	--------------------------------------- Names Shown In The Purchase Clothing Menu ---------------------------------------
+    Hats = "Sombreros",
+    Glasses = "Lentes",
+    Masks = "Máscaras",
+    Neckwear = "Collares",
+    Ties = "Corbatas",
+    Shirts = "Camisas",
+    Suspenders = "Tirantes",
+    Vests = "Chalecos",
+    Coats = "Abrigos",
+    CoatsClosed = "Abrigos cerrados",
+    Ponchos = "Ponchos",
+    Cloaks = "Capas",
+    Gloves = "Guantes",
+    RightRings = "Anillos de la mano derecha",
+    LeftRings = "Anillos de la mano izquierda",
+    Bracelets = "Pulseras",
+    PrimaryHolsters = "Fundas primarias",
+    Belts = "Correas",
+    Buckles = "Hebillas",
+    SecondaryHolsters = "Fundas secundarias",
+    Pants = "Pantalones",
+    Skirts = "Faldas",
+    Chaps = "Chaps",
+    Boots = "Botas",
+    Spurs = "Espuelas",
+    Spats = "Polainas",
+    Gauntlets = "Guanteletes",
+    Loadouts = "Cargas",
+    Accessories = "Accesorios",
+    Satchels = "Carteras",
+    GunbeltAccs = "Accesorios de cinturón",
+
+	--------------------------------------- Descriptions Of The Clothing Menu Sections ---------------------------------------
+    Descriptions = {
+		Hats = "Elige el sombrero",
+		Glasses = "Elige las gafas",
+		Masks = "Elige si quieres llevar mascarilla",
+		Neckwear = "Elige la corbata",
+		Ties = "Elige la corbata",
+		Shirts = "Elige la camisa",
+		Suspenders = "Elige los tirantes",
+		Vests = "Elige el chaleco",
+		Coats = "Elige el abrigo",
+		CoatsClosed = "Elige el abrigo",
+		Ponchos = "Elige el poncho",
+		Cloaks = "Elige la capa",
+		Gloves = "Elige los guantes",
+		RightRings = "Elija los anillos de la mano derecha",
+		LeftRings = "Elija los anillos de la mano izquierda",
+		Bracelets = "Elige la pulsera",
+		PrimaryHolsters = "Elija la funda principal",
+		Belts = "Elige el cinturón",
+		Buckles = "Elige la hebilla",
+		SecondaryHolsters = "Elija la funda secundaria",
+		Pants = "Elige los pantalones",
+		Skirts = "Elige la falda",
+		Chaps = "Elige las chaps",
+		Boots = "Elige las botas",
+		Spurs = "Elige las espuelas",
+		Spats = "Elige las polainas",
+		Gauntlets = "Elige los guanteletes",
+		Loadouts = "Elige las cargas",
+		Accessories = "Elige los accesorios",
+		Satchels = "Elige la cartera",
+		GunbeltAccs = "elegir el cinturón de armas accs",
+	}
+}
