3728822ffc930f073b6d80521b20fdb9ad8372f6
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2022-11-29 23:29:19 -0500

    mapcolor

---
+++ resources/[vorp_plugins]/[external]/1880rp_mapcolor/config.lua
@@ -0,0 +1,33 @@
+--DO NOT RENAME THE RESSOURCE !!
+
+Config = {}
+
+Config.KeyEnable = false
+Config.Key = 0x17BEC168 --E
+
+Config.Region = {
+---STATE
+    --AMBARINO
+    { zone = 0x3B8DD21A, color = 'BLIP_STYLE_TURRET_WEAPON' },
+    --NEW AUSTIN
+    { zone = 0x41759831, color = 'BLIP_STYLE_MP_MISSION_GIVER' },
+    --WEST ELIZABETH
+    { zone = 0xD69B5B49, color = 'BLIP_STYLE_DEBUG_YELLOW' },
+    --NEW HANOVER
+    { zone = 0x41332496, color = 'BLIP_STYLE_FM_EVENT' },
+    --LEMOYNE
+    { zone = 0x945395DF, color = 'BLIP_STYLE_DEBUG_GREEN' },
+
+---REGION
+    --WAPITI RESERVE
+    { zone = 0xBB785C8A, color = 'BLIP_STYLE_MP_CORPSE_FADE' },
+
+--WANTED ZONE
+    --SISIKA
+    { zone = 0x2D1A7AF2, color = 'BLIP_STYLE_WANTED_REGION' },
+    --VanHorn
+    { zone = 0x507B5360, color = 'BLIP_STYLE_WANTED_REGION' },
+}
+
+-- FOR ADD MORE ZONE > https://github.com/femga/rdr3_discoveries/tree/master/graphics/minimap/wanted_regions
+-- FOR COLOR > https://github.com/femga/rdr3_discoveries/tree/master/useful_info_from_rpfs/blip_styles
