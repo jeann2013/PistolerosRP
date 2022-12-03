5e15888bc62dcb9c410c8c63642a886515186877
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2022-12-03 18:20:20 -0500

    add

---
+++ resources/[gamemodes]/[maps]/casa-madrugada-mexico/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "adamant"
+game "rdr3"
+
+this_is_a_map "yes"
+
+files {
+    "stream/CASAMADRUGADA.ymap",
+    "vegetationmodifiers.meta"   
+  }
+
+
+
+rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
