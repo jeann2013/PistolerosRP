12f9e8d0fb7bb7769a9965f9bc304b6c6e796ef2
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2022-12-03 19:18:50 -0500

    add

--- resources/[gamemodes]/[maps]/casa-madrugada-mexico/fxmanifest.lua
+++ resources/[gamemodes]/[maps]/casa-madrugada-mexico/fxmanifest.lua
@@ -3,8 +3,6 @@ game "rdr3"
 
 this_is_a_map "yes"
 
-files {
-    "vegetationmodifiers.meta"
-}
+file 'vegetationmodifiers.meta'
 
 rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
