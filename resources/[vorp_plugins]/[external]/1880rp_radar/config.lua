4057bdbf866d8891479c34242d983bada72c5e7e
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2022-12-06 01:46:54 -0500

    add

---
+++ resources/[vorp_plugins]/[external]/1880rp_radar/config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+Config.DisableRadarOnFoot = false
+
+
+
+Config.RadarType = 3
+--[[
+    0 = Off,
+    1 = Regular (Regular Radar),
+    2 = Expanded (Big Radar),
+    3 = Simple (Compass like), 
+]]
