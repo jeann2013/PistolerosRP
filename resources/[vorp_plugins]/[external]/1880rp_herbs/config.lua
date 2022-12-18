9d8945771abf1e9a583ce1e4e3d583ea06ca11d0
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2022-12-18 14:46:01 -0500

    add

---
+++ resources/[vorp_plugins]/[external]/1880rp_herbs/config.lua
@@ -0,0 +1,25 @@
+Config = {}
+
+Config.SearchKey = 0xD9D0E1C0 -- Key to press for prompt
+
+Config.Items = {
+	{item = "Wintergreen_Berry", name = "Baya Wintergreen"},
+	{item = "Red_Raspberry", name = "Frambuesa roja"},
+	{item = "Black_Berry", name = "Brombere"},
+	{item = "currant", name = "Johannisbeere"},
+	{item = "blueberry", name = "Arándanos"},
+	{item = "Black_Currant", name = "Suroeste de Johannisbeere"},
+	{item = "consumable_herb_evergreen_huckleberry", name = "Baya de hoja perenne"}
+}
+
+Config.randomgive = math.random(1,3)
+
+Config.Language = {
+	prompt = "Buscar",
+	promptsub = "Arbusto",
+	
+	notifytitel = "Berries",
+	
+	notfound = "¡No encontraste bayas!"
+
+}
