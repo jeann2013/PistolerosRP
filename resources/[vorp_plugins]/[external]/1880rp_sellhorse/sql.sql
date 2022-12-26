bfbeb9c93e9dd847259b57a7ded2af29492bc0de
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2022-12-26 00:08:24 -0500

    add

---
+++ resources/[vorp_plugins]/[external]/1880rp_sellhorse/sql.sql
@@ -0,0 +1,5 @@
+  CREATE TABLE IF NOT EXISTS `sellhorse` (
+  `identifier` varchar(100) NOT NULL DEFAULT '0',  
+  `characterid` varchar(5) NOT NULL DEFAULT '0',
+  `time_m` varchar(100) NOT NULL DEFAULT '0'  
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
