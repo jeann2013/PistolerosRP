444f9d5692775a77366a8d0cfff73b391becf89e
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2022-12-26 02:23:22 -0500

    add

--- resources/[vorp_plugins]/vorp_housing/fxmanifest.lua
+++ resources/[vorp_plugins]/vorp_housing/fxmanifest.lua
@@ -6,7 +6,7 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 client_scripts { 'vorphousing_cl.net.dll' }
 server_scripts { 'vorphousing_sv.net.dll' }
 
-files { 'Newtonsoft.Json.dll' }
+files { 'Newtonsoft.Json.dll', 'Config.json' }
 
 -- Log Levels; none, trace, debug, info, warn, error, all
 log_level 'none'
