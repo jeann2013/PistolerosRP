6def3375c291e6d477e88ea017c87daeb92934d8
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2022-12-25 23:47:55 -0500

    add

--- resources/[vorp_essentials]/vorp_inventory/sql/weapons.sql
+++ resources/[vorp_essentials]/vorp_inventory/sql/weapons.sql
@@ -123,6 +123,8 @@ INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VA
 INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('consumable_breakfast', 'Breakfast', 20, 1, 'item_standard', 1);
 INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('consumable_meat_mature_venison_cooked', 'Meat', 20, 1, 'item_standard', 1);
 INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('consumable_meat_stringy_cooked', 'Meat stringy cooked', 20, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('consumable_applepie', 'Apple Pie', 20, 1, 'item_standard', 1);
+
 
 
 
