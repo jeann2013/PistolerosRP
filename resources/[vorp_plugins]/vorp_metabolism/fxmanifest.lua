c982b29ea337b0b88f037ee1615683367625a0f2
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2022-12-28 23:06:17 -0500

    add

--- resources/[vorp_plugins]/vorp_metabolism/fxmanifest.lua
+++ resources/[vorp_plugins]/vorp_metabolism/fxmanifest.lua
@@ -1,17 +1,18 @@
-game 'rdr3'
 fx_version 'adamant'
+game 'rdr3'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-client_scripts {
-  'vorpmetabolism-cl.net.dll',
-}
+lua54 'yes'
 
+shared_script 'config.lua'
+client_scripts {
+  'client/*.lua'
+}
 server_scripts {
-  'vorpmetabolism-sv.net.dll'
+  'server/*.lua'
 }
 
 files {
-  'Newtonsoft.Json.dll',
   'ui/hud.html',
   'ui/css/style.css',
   'ui/js/circle-progress.js',
@@ -21,10 +22,10 @@ files {
   'ui/img/water.png',
   'ui/img/water1.png',
 }
-
+}
 ui_page 'ui/hud.html'
 
 version '1.0'
 vorp_checker 'yes'
 vorp_name '^4Resource version Check^3'
-vorp_github 'https://github.com/VORPCORE/VORP-Metabolism'
+vorp_github 'https://github.com/VORPCORE/vorp_metabolism-lua'
