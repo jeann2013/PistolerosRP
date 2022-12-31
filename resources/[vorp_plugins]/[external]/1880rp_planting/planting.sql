b6fddb2594132f7174b2de5bac43d2a61e93712e
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2022-12-31 16:44:45 -0500

    add

--- resources/[vorp_plugins]/[external]/1880rp_planting/planting.sql
+++ resources/[vorp_plugins]/[external]/1880rp_planting/planting.sql
@@ -1,7 +1,39 @@
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('corn', 'Maiz', -1, 1, 'item_standard', 0);
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('cornseed', 'Semilla de maiz', -1, 1, 'item_standard', 1);
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('sugar', 'Caña de azucar', -1, 1, 'item_standard', 0);
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('sugarcaneseed', 'Semilla de caña de azucar', -1, 1, 'item_standard', 1);
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('tobacco', 'Tabaco', -1, 1, 'item_standard', 0);
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('tobaccoseed', 'Semilla de tabaco', -1, 1, 'item_standard', 1);
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('wateringcan', 'Regadera', 1, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('corn', 'Maiz', 100, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('cornseed', 'Semilla de maiz', 100, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('sugar', 'Caña de azucar', 100, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('sugarcaneseed', 'Semilla de caña de azucar', 100, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('tobacco', 'Tabaco', 100, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('tobaccoseed', 'Semilla de tabaco', 100, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('wateringcan', 'Regadera', 100, 1, 'item_standard', 1);
+
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('lettuceseed', 'Semilla de lechuga', 100, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('lettuce', 'Lechuga', 100, 1, 'item_standard', 1);
+
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('tomatoesseed', 'Semilla de tomate', 100, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('tomatoes', 'Tomate', 100, 1, 'item_standard', 1);
+
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('broccolisseed', 'Semilla de brocoli', 100, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('broccoli', 'Brocoli', 100, 1, 'item_standard', 1);
+
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('weatsseed', 'Semilla de trigo', 100, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('weat', 'Trigo', 100, 1, 'item_standard', 1);
+
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('potatosseed', 'Semilla de papa', 100, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('potato', 'Papa', 100, 1, 'item_standard', 1);
+
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('oreganosseed', 'Semilla de oregano', 100, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('oregano', 'Oregano', 100, 1, 'item_standard', 1);
+
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('yuccasseed', 'Semilla de yuca', 100, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('yucca', 'Yuca', 100, 1, 'item_standard', 1);
+
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('berrysseed', 'Semilla de arandano', 100, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('berry', 'Arandano', 100, 1, 'item_standard', 1);
+
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('cottonsseed', 'Semilla de algodon', 100, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('cotton', 'Algodon', 100, 1, 'item_standard', 1);
+
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('ginsengsseed', 'Semilla de ginseng', 100, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('ginseng', 'Ginseng', 100, 1, 'item_standard', 1);
+
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('carrotsseed', 'Semilla de zanahoria', 100, 1, 'item_standard', 1);
--- resources/[vorp_plugins]/[external]/1880rp_planting/server/server.lua
+++ resources/[vorp_plugins]/[external]/1880rp_planting/server/server.lua
@@ -16,8 +16,66 @@ Citizen.CreateThread(function()
 		Inventory.subItem(data.source, "cornseed", 1)
 		TriggerClientEvent('poke_planting:planto1', data.source, "CRP_CORNSTALKS_CB_SIM", "CRP_CORNSTALKS_CA_SIM", "CRP_CORNSTALKS_AB_SIM")
 	end)
+
+	Inventory.RegisterUsableItem("lettuceseed", function(data)
+		Inventory.subItem(data.source, "lettuceseed", 1)
+		TriggerClientEvent('poke_planting:planto1', data.source, "CRP_LETTUCE_AA_SIM", "CRP_LETTUCE_AA_SIM", "CRP_LETTUCE_AA_SIM")
+	end)
+
+	Inventory.RegisterUsableItem("tomatoesseed", function(data)
+		Inventory.subItem(data.source, "tomatoesseed", 1)
+		TriggerClientEvent('poke_planting:planto1', data.source, "CRP_TOMATOES_SAP_AA_SIM", "CRP_TOMATOES_HAR_AA_SIM", "CRP_TOMATOES_AA_SIM")
+	end)
+
+	Inventory.RegisterUsableItem("broccolisseed", function(data)
+		Inventory.subItem(data.source, "broccolisseed", 1)
+		TriggerClientEvent('poke_planting:planto1', data.source, "CRP_BROCCOLI_AA_SIM", "CRP_BROCCOLI_AA_SIM", "CRP_BROCCOLI_AA_SIM")
+	end)
+
+	Inventory.RegisterUsableItem("weatsseed", function(data)
+		Inventory.subItem(data.source, "weatsseed", 1)
+		TriggerClientEvent('poke_planting:planto1', data.source, "CRP_WHEAT_SAP_LONG_AA_SIM", "CRP_WHEAT_SAP_LONG_AB_SIM", "CRP_WHEAT_DRY_LONG_AA_SIM")
+	end)
+
+	Inventory.RegisterUsableItem("potatosseed", function(data)
+		Inventory.subItem(data.source, "potatosseed", 1)
+		TriggerClientEvent('poke_planting:planto1', data.source, "CRP_POTATO_SAP_AA_SIM", "CRP_POTATO_SAP_AA_SIM", "CRP_POTATO_AA_SIM")
+	end)
+
+	Inventory.RegisterUsableItem("oreganosseed", function(data)
+		Inventory.subItem(data.source, "oreganosseed", 1)
+		TriggerClientEvent('poke_planting:planto1', data.source, "OREGANO_P", "OREGANO_P", "OREGANO_P")
+	end)
+
+	Inventory.RegisterUsableItem("yuccasseed", function(data)
+		Inventory.subItem(data.source, "yuccasseed", 1)
+		TriggerClientEvent('poke_planting:planto1', data.source, "RDR_BUSH_YUCCA_AA_SIM", "RDR_BUSH_YUCCA_AA_SIM", "RDR_BUSH_YUCCA_DEAD_AA_SIM")
+	end)
+
+	Inventory.RegisterUsableItem("berrysseed", function(data)
+		Inventory.subItem(data.source, "berrysseed", 1)
+		TriggerClientEvent('poke_planting:planto1', data.source, "CRP_BERRY_HAR_AA_SIM", "CRP_BERRY_HAR_AA_SIM", "CRP_BERRY_AA_SIM")
+	end)
+
+	Inventory.RegisterUsableItem("ginsengsseed", function(data)
+		Inventory.subItem(data.source, "ginsengsseed", 1)
+		TriggerClientEvent('poke_planting:planto1', data.source, "CRP_GINSENG_AB_SIM", "CRP_GINSENG_AA_SIM", "CRP_GINSENG_BA_SIM")
+	end)
+
+	Inventory.RegisterUsableItem("cottonsseed", function(data)
+		Inventory.subItem(data.source, "cottonsseed", 1)
+		TriggerClientEvent('poke_planting:planto1', data.source, "CRP_COTTON_BA_SIM", "CRP_COTTON_BB_SIM", "CRP_COTTON_BE_SIM")
+	end)
+
+	Inventory.RegisterUsableItem("carrotsseed", function(data)
+		Inventory.subItem(data.source, "carrotsseed", 1)
+		TriggerClientEvent('poke_planting:planto1', data.source, "CRP_CARROTS_SAP_BA_SIM", "CRP_CARROTS_SAP_BA_SIM", "CRP_CARROTS_BA_SIM")
+	end)
+
+	
 	
 	Inventory.RegisterUsableItem("wateringcan", function(data)
+		Inventory.subItem(data.source, "wateringcan", 1)
 		TriggerClientEvent('poke_planting:regar1', data.source)
 	end)
 end)
@@ -35,5 +93,39 @@ AddEventHandler('poke_planting:giveitem', function(tipo)
 	elseif tipo == "CRP_CORNSTALKS_AB_SIM" then
 		TriggerClientEvent("vorp:TipRight", _source, 'Has recolectado [X'..count..'] de maiz', 3000)
 		Inventory.addItem(_source, "corn", count)
+	elseif tipo == "CRP_LETTUCE_AA_SIM" then
+		TriggerClientEvent("vorp:TipRight", _source, 'Has recolectado [X'..count..'] de lechuga', 3000)
+		Inventory.addItem(_source, "lettuce", count)
+	elseif tipo == "CRP_TOMATOES_AA_SIM" then
+		TriggerClientEvent("vorp:TipRight", _source, 'Has recolectado [X'..count..'] de tomates', 3000)
+		Inventory.addItem(_source, "tomatoes", count)
+	elseif tipo == "CRP_BROCCOLI_AA_SIM" then
+		TriggerClientEvent("vorp:TipRight", _source, 'Has recolectado [X'..count..'] de brocoli', 3000)
+		Inventory.addItem(_source, "broccoli", count)
+	elseif tipo == "CRP_WHEAT_DRY_LONG_AA_SIM" then
+		TriggerClientEvent("vorp:TipRight", _source, 'Has recolectado [X'..count..'] de trigo', 3000)
+		Inventory.addItem(_source, "wheat", count)
+	elseif tipo == "CRP_POTATO_AA_SIM" then
+		TriggerClientEvent("vorp:TipRight", _source, 'Has recolectado [X'..count..'] de papa', 3000)
+		Inventory.addItem(_source, "potato", count)
+	elseif tipo == "OREGANO_P" then
+		TriggerClientEvent("vorp:TipRight", _source, 'Has recolectado [X'..count..'] de oregano', 3000)
+		Inventory.addItem(_source, "oregano", count)
+	elseif tipo == "RDR_BUSH_YUCCA_DEAD_AA_SIM" then
+		TriggerClientEvent("vorp:TipRight", _source, 'Has recolectado [X'..count..'] de yuca', 3000)
+		Inventory.addItem(_source, "yucca", count)
+	elseif tipo == "CRP_BERRY_AA_SIM" then
+		TriggerClientEvent("vorp:TipRight", _source, 'Has recolectado [X'..count..'] de arandano', 3000)
+		Inventory.addItem(_source, "berry", count)
+	elseif tipo == "CRP_GINSENG_BA_SIM" then
+		TriggerClientEvent("vorp:TipRight", _source, 'Has recolectado [X'..count..'] de ginseng', 3000)
+		Inventory.addItem(_source, "ginseng", count)
+	elseif tipo == "CRP_COTTON_BE_SIM" then
+		TriggerClientEvent("vorp:TipRight", _source, 'Has recolectado [X'..count..'] de algodon', 3000)
+		Inventory.addItem(_source, "cotton", count)		
+	elseif tipo == "CRP_CARROTS_BA_SIM" then
+		TriggerClientEvent("vorp:TipRight", _source, 'Has recolectado [X'..count..'] de zanahoria', 3000)
+		Inventory.addItem(_source, "carrots", count)
+
 	end
 end)
