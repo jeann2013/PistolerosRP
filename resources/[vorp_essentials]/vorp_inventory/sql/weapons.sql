be4d99d526577cca989116527274bb21657ecae4
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2022-12-31 21:47:45 -0500

    add

--- resources/[vorp_essentials]/vorp_inventory/sql/weapons.sql
+++ resources/[vorp_essentials]/vorp_inventory/sql/weapons.sql
@@ -138,6 +138,8 @@ INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VA
 INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('coffeeseed', 'Semilla de Cafe', 100, 1, 'item_standard', 1);
 INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('coffeegrinder', 'Molino de Cafe', 100, 1, 'item_standard', 1);
 INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('groundcoffee', 'Cafe molido', 100, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('groundcoffee', 'Cafe molido', 100, 1, 'item_standard', 1);
+
 
 
 
