ce4c1c7d38395461c48bb2962c8234427f320b5a
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2023-01-03 16:46:16 -0500

    add

--- resources/[vorp_plugins]/vorp_housing/fxmanifest.lua
+++ resources/[vorp_plugins]/vorp_housing/fxmanifest.lua
@@ -1,12 +1,12 @@
-game 'rdr3'
-
 fx_version 'cerulean'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-client_scripts { '*.Client.net.dll', 'Config.json' }
-server_scripts { '*.Server.net.dll', 'Config.json' }
+game 'rdr3'
 
-files { 'Newtonsoft.Json.dll', 'Config.json', 'languages/**/*' }
+client_scripts { '*.Client.net.dll', 'config.json' }
+server_scripts { '*.Server.net.dll', 'config.json' }
+
+files { 'Newtonsoft.Json.dll', 'config.json', 'languages/**/*' }
 
 -- Log Levels; none, trace, debug, info, warn, error, all
 log_level 'none'
